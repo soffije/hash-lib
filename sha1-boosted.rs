5ff525b3a42464f18130ebbfef2f1d6d6b9dc130
sonia <93443981+soffije@users.noreply.github.com>
2023-06-03 01:40:03 +0300

    Update sha1-boosted.rs

--- sha1-boosted.rs
+++ sha1-boosted.rs
@@ -1,26 +1,25 @@
 extern crate crypto;
+extern crate rayon;
 
 use crypto::digest::Digest;
 use crypto::sha1::Sha1;
-
+use rayon::prelude::*;
 use std::time::Instant;
 
 const BLOCK_SIZE: usize = 64;
 
+// Функція для додавання доповнення до повідомлення
 fn pad_message(message: &[u8]) -> Vec<u8> {
     let message_len = message.len();
     let bit_len = (message_len as u64) * 8;
     let mut padded = Vec::from(message);
 
-    // Додаємо одиницю '1' у бітовому представленні
     padded.push(0x80);
 
-    // Додаємо '0' біти до тих пір, поки довжина повідомлення в бітах ≡ 448 (mod 512)
     while (padded.len() % BLOCK_SIZE) != (BLOCK_SIZE - 8) {
         padded.push(0);
     }
 
-    // Додаємо початкову довжину повідомлення у бітах, у вигляді 64-бітного цілого числа в порядку big-endian
     padded.extend(&[(bit_len >> 56) as u8]);
     padded.extend(&[(bit_len >> 48) as u8]);
     padded.extend(&[(bit_len >> 40) as u8]);
@@ -33,79 +32,67 @@ fn pad_message(message: &[u8]) -> Vec<u8> {
     padded
 }
 
-fn hash_sha1(message: &[u8]) -> [u8; 20] {
-    let padded_message = pad_message(message);
-    let block_count = padded_message.len() / BLOCK_SIZE;
-
+// Функція для хешування одного блоку повідомлення за алгоритмом SHA-1
+fn hash_sha1_block(block: &[u8]) -> [u8; 20] {
+    // Ініціалізуємо початкові значення хеш-функції
     let mut h0: u32 = 0x67452301;
     let mut h1: u32 = 0xEFCDAB89;
     let mut h2: u32 = 0x98BADCFE;
     let mut h3: u32 = 0x10325476;
     let mut h4: u32 = 0xC3D2E1F0;
 
-    for i in 0..block_count {
-        let block = &padded_message[i * BLOCK_SIZE..(i + 1) * BLOCK_SIZE];
-        let mut words = [0u32; 80];
+    let mut words = [0u32; 80];
 
-        // Ініціалізуємо перші 16 слів даними блоку
-        for j in 0..16 {
-            let offset = j * 4;
-            words[j] = ((block[offset] as u32) << 24)
-                | ((block[offset + 1] as u32) << 16)
-                | ((block[offset + 2] as u32) << 8)
-                | block[offset + 3] as u32;
+    // Розбиваємо блок на 16 слів по 32 біти (4 байти)
+    for j in 0..16 {
+        let offset = j * 4;
+        words[j] = ((block[offset] as u32) << 24)
+            | ((block[offset + 1] as u32) << 16)
+            | ((block[offset + 2] as u32) << 8)
+            | block[offset + 3] as u32;
+    }
+
+    // Генеруємо 64 нових слів на основі попередніх 16 слів
+    for j in 16..80 {
+        let w = words[j - 3] ^ words[j - 8] ^ words[j - 14] ^ words[j - 16];
+        words[j] = w.rotate_left(1);
+    }
+
+    let mut a = h0;
+    let mut b = h1;
+    let mut c = h2;
+    let mut d = h3;
+    let mut e = h4;
+
+    // Виконуємо 80 ітерацій обробки повідомлення
+    for j in 0..80 {
+        let f;
+        let k;
+        if j < 20 {
+            f = (b & c) | ((!b) & d);
+            k = 0x5A827999;
+        } else if j < 40 {
+            f = b ^ c ^ d;
+            k = 0x6ED9EBA1;
+        } else if j < 60 {
+            f = (b & c) | (b & d) | (c & d);
+            k = 0x8F1BBCDC;
+        } else {
+            f = b ^ c ^ d;
+            k = 0xCA62C1D6;
         }
 
-        // Розширюємо решту слів
-        for j in 16..80 {
-            let w = words[j - 3] ^ words[j - 8] ^ words[j - 14] ^ words[j - 16];
-            words[j] = w.rotate_left(1);
-        }
+        let temp = a.rotate_left(5)
+            .wrapping_add(f)
+            .wrapping_add(e)
+            .wrapping_add(words[j])
+            .wrapping_add(k);
 
-        // Ініціалізуємо значення хешу для цього блоку
-        let mut a = h0;
-        let mut b = h1;
-        let mut c = h2;
-        let mut d = h3;
-        let mut e = h4;
-
-        // Основний цикл
-        for j in 0..80 {
-            let f;
-            let k;
-            if j < 20 {
-                f = (b & c) | ((!b) & d);
-                k = 0x5A827999;
-            } else if j < 40 {
-                f = b ^ c ^ d;
-                k = 0x6ED9EBA1;
-            } else if j < 60 {
-                f = (b & c) | (b & d) | (c & d);
-                k = 0x8F1BBCDC;
-            } else {
-                f = b ^ c ^ d;
-                k = 0xCA62C1D6;
-            }
-
-            let temp = a.rotate_left(5)
-                .wrapping_add(f)
-                .wrapping_add(e)
-                .wrapping_add(words[j])
-                .wrapping_add(k);
-
-            e = d;
-            d = c;
-            c = b.rotate_left(30);
-            b = a;
-            a = temp;
-        }
-
-        // Оновлюємо значення хешу для цього блоку
-        h0 = h0.wrapping_add(a);
-        h1 = h1.wrapping_add(b);
-        h2 = h2.wrapping_add(c);
-        h3 = h3.wrapping_add(d);
-        h4 = h4.wrapping_add(e);
+        e = d;
+        d = c;
+        c = b.rotate_left(30);
+        b = a;
+        a = temp;
     }
 
     let mut result = [0u8; 20];
@@ -129,6 +116,28 @@ fn hash_sha1(message: &[u8]) -> [u8; 20] {
     result[17] = (h4 >> 16) as u8;
     result[18] = (h4 >> 8) as u8;
     result[19] = h4 as u8;
+
+    result
+}
+
+// Функція для хешування повідомлення за алгоритмом SHA-1
+fn hash_sha1(message: &[u8]) -> [u8; 20] {
+    let padded_message = pad_message(message);
+    let block_count = padded_message.len() / BLOCK_SIZE;
+
+    // Розбиваємо повідомлення на блоки та паралельно обчислюємо хеш кожного блоку
+    let hashes: Vec<[u8; 20]> = padded_message
+        .par_chunks(BLOCK_SIZE)
+        .map(|block| hash_sha1_block(block))
+        .collect();
+
+    let mut result = [0u8; 20];
+    // Об'єднуємо хеші блоків в кінцевий хеш повідомлення
+    for hash in hashes {
+        for i in 0..20 {
+            result[i] ^= hash[i];
+        }
+    }
 
     result
 }
